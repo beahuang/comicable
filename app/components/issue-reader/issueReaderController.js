0aaf8f6aaec301b830e0a0185063b1cba6d61b22
Beatrice Huang <beahuang1@gmail.com>
2016-04-08 19:43:05 -0400

    issue re reader functionality

--- app/components/issue-reader/issueReaderController.js
+++ app/components/issue-reader/issueReaderController.js
@@ -1,3 +1,25 @@
 comicableApp.controller( 'issueReaderController', function( $scope ) {
     $scope.message = 'Issue Reader'
+
+    $scope.imageUrl = 'images/batman-cover.jpg';
+    $scope.counter = 0;
+    $scope.maxPages = 30;
+    $scope.nextImage = function() {
+        if ($scope.counter >= $scope.maxPages) {
+            $scope.imageUrl = 'images/batman-cover.jpg';
+            $scope.counter = 0;
+        } else {
+            $scope.counter += 1;
+            $scope.imageUrl = 'images/batman-' + $scope.counter + '.jpg';
+        }
+    };
+
+    $scope.prevImage = function() {
+        if ($scope.counter <= 1) {
+            $scope.imageUrl = 'images/batman-cover.jpg';
+        } else {
+            $scope.counter -= 1;
+            $scope.imageUrl = 'images/batman-' + $scope.counter + '.jpg';
+        }
+    };
 })
--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -59,6 +59,28 @@ comicableApp.controller( 'currentlyReadingController', function( $scope ) {
 
 comicableApp.controller( 'issueReaderController', function( $scope ) {
     $scope.message = 'Issue Reader'
+
+    $scope.imageUrl = 'images/batman-cover.jpg';
+    $scope.counter = 0;
+    $scope.maxPages = 30;
+    $scope.nextImage = function() {
+        if ($scope.counter >= $scope.maxPages) {
+            $scope.imageUrl = 'images/batman-cover.jpg';
+            $scope.counter = 0;
+        } else {
+            $scope.counter += 1;
+            $scope.imageUrl = 'images/batman-' + $scope.counter + '.jpg';
+        }
+    };
+
+    $scope.prevImage = function() {
+        if ($scope.counter <= 1) {
+            $scope.imageUrl = 'images/batman-cover.jpg';
+        } else {
+            $scope.counter -= 1;
+            $scope.imageUrl = 'images/batman-' + $scope.counter + '.jpg';
+        }
+    };
 })
 
 var comicableApp = angular.module( 'comicableApp' );
