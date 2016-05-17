d9b344f7ab41e45ee83f12c3e813cda989e630fe
Beatrice Huang <beahuang1@gmail.com>
2016-05-16 20:48:37 -0400

    fix some syntax and remove autoupdate of mongoose

--- app/server.js
+++ app/server.js
@@ -16,11 +16,9 @@ server.start( err => {
 });
 
 const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
 const url = 'mongodb://localhost:27017/comicable';
 
-MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err);
-  console.log("Connected correctly to server.");
+MongoClient.connect( url, ( err, db ) => {
+  console.log('Connected to the database');
   db.close();
 });
