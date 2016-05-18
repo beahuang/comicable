d523a7b365af18bb93baf3ccbf448cfd3cb99064
alfred <aababio75@gmail.com>
2016-05-18 16:44:27 -0400

    Tests for the two sync methods were easy and exist

---
+++ test/fixtures/mockHTML.js
@@ -0,0 +1,46 @@
+'use strict';
+
+module.exports = {
+  pageNumberHTML  : `<div id="items">
+                      <span class="browse-item"><b>1</b></span>
+                      <span class="browse-item"><a href="?start=20">2</a></span>
+                      <span class="browse-item"><a href="?start=40">3</a></span>
+                      <span class="browse-item"><a href="?start=60">4</a></span>
+                      <span class="browse-item"><a href="?start=20">Next</a></span>
+                      </div>`,
+
+  issuesHTML      : [`<tr>
+                      <td id="image">
+                        <a href="/sku/MAR160802/Agents-of-S-H-I-E-L-D-2016-5-Aso">
+                          <img src="https://images-na.ssl-images-amazon.com/images/S/cmx-images-prod/PrintItem/501600/MAR160802._SX200_QL80_TTD_.jpg" class="img-full">
+                        </a>
+                      </td>
+                      <td id="synopsis">
+                        <div id="title">
+                          <span id="icon">
+                            <a href="/marvel/2016/05/11">
+                              <img src="https://images-na.ssl-images-amazon.com/images/S/cmx-images-prod/Publisher/2/15_tile_img_full_0_1_2_3._SS32_QL80_TTD_.jpg" width="16" height="16">
+                            </a>
+                          </span>&nbsp;
+                          <a href="/sku/MAR160802/Agents-of-S-H-I-E-L-D-2016-5-Aso">Agents of S.H.I.E.L.D. (2016-) #5 Aso</a>
+                        </div>
+                      </td>
+                    </tr>`,
+                    `<tr>
+                      <td id="image">
+                        <a href="/sku/MAR160189/Constantine-The-Hellblazer-2015-12">
+                          <img src="https://images-na.ssl-images-amazon.com/images/S/cmx-images-prod/PrintItem/500366/MAR160189._SX200_QL80_TTD_.jpg" class="img-full">
+                        </a>
+                      </td>
+                      <td id="synopsis">
+                        <div id="title">
+                          <span id="icon">
+                            <a href="/dc_comics/2016/05/11">
+                            <img src="https://images-na.ssl-images-amazon.com/images/S/cmx-images-prod/Publisher/1/dc_comics_125x125._SS32_QL80_TTD_.png">
+                            </a>
+                          </span>&nbsp;
+                          <a href="/sku/MAR160189/Constantine-The-Hellblazer-2015-12">Constantine: The Hellblazer (2015-) #12</a>
+                        </div>
+                      </td>
+                    </tr>`]
+};
