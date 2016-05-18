e1a587fc5c359e80bcc424eab402734a5e92ccff
Alfred Ababio <aababio75@gmail.com>
2016-05-18 19:22:55 -0400

    getIssues tests written

--- test/fixtures/mockHTML.js
+++ test/fixtures/mockHTML.js
@@ -9,38 +9,48 @@ module.exports = {
                       <span class="browse-item"><a href="?start=20">Next</a></span>
                       </div>`,
 
-  issuesHTML      : [`<tr>
-                      <td id="image">
-                        <a href="/sku/MAR160802/Agents-of-S-H-I-E-L-D-2016-5-Aso">
-                          <img src="https://images-na.ssl-images-amazon.com/images/S/cmx-images-prod/PrintItem/501600/MAR160802._SX200_QL80_TTD_.jpg" class="img-full">
-                        </a>
-                      </td>
-                      <td id="synopsis">
-                        <div id="title">
-                          <span id="icon">
-                            <a href="/marvel/2016/05/11">
-                              <img src="https://images-na.ssl-images-amazon.com/images/S/cmx-images-prod/Publisher/2/15_tile_img_full_0_1_2_3._SS32_QL80_TTD_.jpg" width="16" height="16">
-                            </a>
-                          </span>&nbsp;
-                          <a href="/sku/MAR160802/Agents-of-S-H-I-E-L-D-2016-5-Aso">Agents of S.H.I.E.L.D. (2016-) #5 Aso</a>
+  issuesHTML      : `<div id="list-items">
+                      <div id="FUCK">
+                        <div id="MAR160802">
+                          <table border="0" cellpadding="0" cellspacing="0" id="listings">
+                            <tbody>
+                              <tr>
+                                <td id="image">
+                                  <a href="/sku/MAR160802/Agents-of-S-H-I-E-L-D-2016-5-Aso">
+                                    <img src="https://images-na.ssl-images-amazon.com/images/S/cmx-images-prod/PrintItem/501600/MAR160802._SX200_QL80_TTD_.jpg" class="img-full">
+                                  </a>
+                                </td>
+                                <td id="synopsis">
+                                  <div id="title">
+                                    <span id="icon">
+                                      <a href="/marvel/2016/05/11">
+                                        <img src="https://images-na.ssl-images-amazon.com/images/S/cmx-images-prod/Publisher/2/15_tile_img_full_0_1_2_3._SS32_QL80_TTD_.jpg" width="16" height="16">
+                                      </a>
+                                    </span>&nbsp;
+                                    <a href="/sku/MAR160802/Agents-of-S-H-I-E-L-D-2016-5-Aso">Agents of S.H.I.E.L.D. (2016-) #5 Aso</a>
+                                  </div>
+                                </td>
+                              </tr>
+                              <tr>
+                                <td id="image">
+                                  <a href="/sku/MAR160189/Constantine-The-Hellblazer-2015-12">
+                                    <img src="https://images-na.ssl-images-amazon.com/images/S/cmx-images-prod/PrintItem/500366/MAR160189._SX200_QL80_TTD_.jpg" class="img-full">
+                                  </a>
+                                </td>
+                                <td id="synopsis">
+                                  <div id="title">
+                                    <span id="icon">
+                                      <a href="/dc_comics/2016/05/11">
+                                        <img src="https://images-na.ssl-images-amazon.com/images/S/cmx-images-prod/Publisher/1/dc_comics_125x125._SS32_QL80_TTD_.png">
+                                      </a>
+                                    </span>&nbsp;
+                                    <a href="/sku/MAR160189/Constantine-The-Hellblazer-2015-12">Constantine: The Hellblazer (2015-) #12</a>
+                                  </div>
+                                </td>
+                              </tr>
+                            </tbody>
+                          </table>
                         </div>
-                      </td>
-                    </tr>`,
-                    `<tr>
-                      <td id="image">
-                        <a href="/sku/MAR160189/Constantine-The-Hellblazer-2015-12">
-                          <img src="https://images-na.ssl-images-amazon.com/images/S/cmx-images-prod/PrintItem/500366/MAR160189._SX200_QL80_TTD_.jpg" class="img-full">
-                        </a>
-                      </td>
-                      <td id="synopsis">
-                        <div id="title">
-                          <span id="icon">
-                            <a href="/dc_comics/2016/05/11">
-                            <img src="https://images-na.ssl-images-amazon.com/images/S/cmx-images-prod/Publisher/1/dc_comics_125x125._SS32_QL80_TTD_.png">
-                            </a>
-                          </span>&nbsp;
-                          <a href="/sku/MAR160189/Constantine-The-Hellblazer-2015-12">Constantine: The Hellblazer (2015-) #12</a>
-                        </div>
-                      </td>
-                    </tr>`]
+                      </div>
+                    </div>`
 };
