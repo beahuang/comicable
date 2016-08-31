85aa4a306642dc39a06e28ef22cb878514789c86
Alfred Ababio <aababio75@gmail.com>
2016-08-31 18:08:23 -0400

    Added inert and added a route that serves statics from /dist

--- app/server.js
+++ app/server.js
@@ -1,24 +1,51 @@
 'use strict';
 
 const Hapi = require('hapi');
-
 const server = new Hapi.Server();
-server.connection( { port: 8081 } );
+const inert = require('inert');
+const Promise = require('bluebird');
 
-require('./api.js')( server );
+server.connection( { port: 8080 } );
 
-server.start( err => {
 
-    if ( err ) {
-        throw err;
+
+require('./routes/api.js')( server );
+
+function register( plugin ) {
+  return new Promise( ( resolve, reject ) => {
+    server.register( plugin, err => {
+      if ( err ) {
+        return reject( err );
+      }
+      resolve();
+    });
+  });
+}
+
+let plugins = [
+  register( inert )
+];
+
+Promise.all( plugins ).then( () => {
+
+  // Serves statics
+  server.route({
+    method: 'GET',
+    path: '/{param*}',
+    handler: {
+      directory: {
+        path: 'dist'
+      }
     }
-    console.log( 'Server running at:', server.info.uri );
-});
+  });
 
-const MongoClient = require('mongodb').MongoClient;
-const url = 'mongodb://localhost:27017/comicable';
+  server.start( err => {
+    if ( err ) {
+      console.error( err ); // Synchronous write to STDERR
+      process.exit( 1 );
+    }
+    console.log('Server started on port: ', server.info.port );
+  });
+})
+.catch( console.error );
 
-MongoClient.connect( url, ( err, db ) => {
-  console.log('Connected to the database');
-  db.close();
-});
